c689a450d1afc2b4655db6ea3c061ee29696013c
Calcifer <tsmpanda@gmail.com>
2018-07-30 15:22:19 +1200

    msg in DotFileAdapter class

--- model/DotFileAdapter.java
+++ model/DotFileAdapter.java
@@ -2,4 +2,7 @@ package model;
 
 //@TODO Corey
 public class  DotFileAdapter {
+    //Hi, could you please also check whether the file path is valid, and throw FileNotFoundException if the filepath
+    //passed to you is not valid? Jenny will catch and handle that exception in Main method. Thanks!
+
 }
