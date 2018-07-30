2ea4638dd65568810e04e93a69411846d0650625
Jenny Lee <jlee923@aucklanduni.ac.nz>
2018-07-31 10:45:50 +1200

    changed system.out to buffer string in notification

--- model/Notification.java
+++ model/Notification.java
@@ -3,14 +3,15 @@ package model;
 public class Notification {
     public static void message(String errorMessage){
         //Better to change this to a String cuz later we'll need to pass this to GUI as a popup
-        System.out.println(errorMessage);
-        System.out.println("java -jar scheduler.jar INPUT.dot P [OPTION]\n");
-        System.out.println("INPUT.dot\ta task graph with integer weights in dot format\n");
-        System.out.println("\n");
-        System.out.println("Optional:\n");
-        System.out.println("-p N\tuse N cores for execution in parallel (default is sequential\n");
-        System.out.println("-v\tvisualise the search\n");
-        System.out.println("-o OUTPUT\toutput file is named OUTPUt ( default is INPUT-output.dot");
+        StringBuffer s = new StringBuffer(errorMessage);
+        s.append("\njava -jar scheduler.jar INPUT.dot P [OPTION]\n");
+        s.append("INPUT.dot\ta task graph with integer weights in dot format\n");
+        s.append("\n");
+        s.append("Optional:\n");
+        s.append("-p N\tuse N cores for execution in parallel (default is sequential\n");
+        s.append("-v\tvisualise the search\n");
+        s.append("-o OUTPUT\toutput file is named OUTPUT ( default is INPUT-output.dot");
+        System.out.println(s);
     }
 
     public static void invalidInput(){
