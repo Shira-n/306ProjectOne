9297efa7f5939c6ca1408d8f2dae568f536e877d
josh-rosairo <32214480+josh-rosairo@users.noreply.github.com>
2018-07-31 00:33:41 +1200

    Added JUnit test file for Node class
    Basic skeleton,  'add' methods tests implemented on a basic level. Will add more detailed tests.

---
+++ model/NodeTest.java
@@ -0,0 +1,63 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class NodeTest {
+	
+	private Node _baseNode;
+	 
+	/* TO DO @Josh */
+	@Before
+	public void initialise() {
+		_baseNode = new Node(1);
+	}
+	
+	/** 
+	 * Tests getWeight() method returns the correct value 
+	 */
+	@Test
+	public void getWeightTest() {
+		assertEquals(_baseNode.getWeight(),1);
+	}
+	
+	/**
+	 *  Tests that addParent() successfully adds a parent to a Node
+	 */
+	@Test
+	public void addParentTest() {
+		/* Makes a node and uses addParent() method to add it as a parent to _baseNode */
+		Node Parent = new Node(1);
+		try {
+			_baseNode.addParent(Parent, 1);
+			assertTrue(_baseNode.isParent(Parent)); 
+		}
+		catch(Exception ex) {
+			fail("Should add 'Parent' as parent node to _baseNode");
+		}
+	}
+	
+	/**
+	 *  Tests that addChild() successfully adds a parent to a Node
+	 */
+	@Test
+	public void addChildTest() {
+		/* Makes a node and uses addChild() method to add it as a child to _baseNode */
+		Node Child = new Node(1);
+		try {
+			_baseNode.addChild(Child, 1);
+			assertTrue(_baseNode.isChild(Child)); 
+		}
+		catch(Exception ex) {
+			fail("Should add 'Child' as child node to _baseNode");
+		}
+	}
+	
+	//@TODO @Josh
+	public void isChildTest() {};
+	public void isParentTest() {};
+	public void getPathWeightToChildTest() {};
+	
+
+}
