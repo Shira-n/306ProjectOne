3aab4c7fba154de608690b0f1660d16f4fbddff5
Calcifer <tsmpanda@gmail.com>
2018-07-31 21:39:29 +1200

    Modified pom.xml. Maven works on Grace's computer

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,4 @@
-package main.java.controller;
+package controller;
 
 public class Controller {
 }
--- src/main/java/model/Notification.java
+++ src/main/java/model/Notification.java
@@ -1,4 +1,4 @@
-package main.java.model;
+package model;
 
 public class Notification {
     public static void message(String errorMessage){
