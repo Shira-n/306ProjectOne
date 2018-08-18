cdf5360b0b3b44ecb25ebef103463ffa039ba1cd
Jenny Lee <jlee923@aucklanduni.ac.nz>
2018-08-18 21:49:27 +1200

    merge conflicts

--- src/main/java/controller/ColourManager.java
+++ src/main/java/controller/ColourManager.java
@@ -14,22 +14,22 @@ public class ColourManager {
 
 
     List<String> colourChoices = new ArrayList<String>() {{
-                add("#39add1"), // light blue
-                add("#3079ab"), // dark blue
-                add("#c25975"), // mauve
-                add("#e15258"), // red
-                add("#f9845b"), // orange
-                add("#838cc7"), // lavender
-                add("#7d669e"), // purple
-                add("#53bbb4"), // aqua
-                add("#51b46d"), // green
-                add("#e0ab18"), // mustard
-                add("#637a91"), // dark gray
-                add("#f092b0"), // pink
-                add("#b7c0c7"),//light grey
-                add("#FCECC9"),//pale yellow
-                add("#C8D5B9"), // pale green
-                add("#3B1C32")}
+                add("#39add1"); // light blue
+                add("#3079ab"); // dark blue
+                add("#c25975"); // mauve
+                add("#e15258"); // red
+                add("#f9845b"); // orange
+                add("#838cc7"); // lavender
+                add("#7d669e"); // purple
+                add("#53bbb4"); // aqua
+                add("#51b46d"); // green
+                add("#e0ab18"); // mustard
+                add("#637a91"); // dark gray
+                add("#f092b0"); // pink
+                add("#b7c0c7");//light grey
+                add("#FCECC9");//pale yellow
+                add("#C8D5B9"); // pale green
+                add("#3B1C32");}
     };
 
     public ColourManager (int numProcessor) {
